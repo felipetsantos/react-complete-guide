e21ef8fc9f4512aca0e99bd493803964258384c7
Felipe Santos <felipe.feteles@gmail.com>
2019-05-04 20:01:58 +0100

    sec:3 - understading the base features and syntax.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 28},
+      { name: 'Stephanie', age: 26},
+    ],
+    other: 'sss'
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28},
+        { name: 'Manu', age: 28},
+        { name: 'Stephanie1', age: 26},
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) =>{
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value, age: 28},
+        { name: 'Stephanie1', age: 26},
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroudColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi I am a react app. {this.state.other}</h1>
+        <button
+        style={style} 
+        onClick={() => this.switchNameHandler('Maximilian!!')}>Swicth Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Max!')}
+        changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}></Person>
       </div>
     );
   }
